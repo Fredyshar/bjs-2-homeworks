99eaf4630dbe8d83707b9ce70b1989c415f2596a
Fedor Sharov <1710193@mail.ru>
2023-03-28 08:20:29 +0300

    Исправлен код по замечаниям

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,21 +4,20 @@ function Student(name, gender, age) {
   this.age = age;
   this.marks = [];
 }
-let student = new Student("Aria", "female", 8);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if ("marks" in student) {
+  if (this.marks !== undefined) {
     this.marks = [...this.marks, ...marks];
     } 
 }
 
 Student.prototype.getAverage = function () {
   let avgMarks = 0;
-  if ("marks" in student === false || this.marks.length === 0 ) {
+  if (this.marks === undefined || this.marks.length === 0 ) {
     return avgMarks;
     } else {
       let sumMarks = (this.marks.reduce((acc, item) => acc + item, 0));
@@ -29,8 +28,8 @@ Student.prototype.getAverage = function () {
 
 Student.prototype.exclude = function (reason) {
   this.excluded = reason;
-  delete student.subject;
-  delete student.marks;
+  delete Student.subject;
+  delete Student.marks;
   delete this.subject;
   delete this.marks;
 }
